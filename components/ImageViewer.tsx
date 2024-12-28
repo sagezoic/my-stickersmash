e47aea8cb707b6996d1af58334c28d40d9eed843
Rakesh Pandey <rakesh.pandey@pattern.com>
2024-12-28 17:27:11 +0530

    minor fix

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function ImageViewer({ imgSource, selectedImage }: Props) {
   const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
 
-  return <Image source={imgSource} style={styles.image} />;
+  return <Image source={imageSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
