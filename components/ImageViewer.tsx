7b179ec5a8a91912c3b0a2f957168ea3d06622cc
Rakesh Pandey <rakesh.pandey@pattern.com>
2024-12-28 17:24:02 +0530

    added the upload image dialog

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -3,9 +3,12 @@ import { Image, type ImageSource } from "expo-image";
 
 type Props = {
   imgSource: ImageSource;
+  selectedImage?: string;
 };
 
-export default function ImageViewer({ imgSource }: Props) {
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+
   return <Image source={imgSource} style={styles.image} />;
 }
 
